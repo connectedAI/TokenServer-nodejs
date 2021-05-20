561f7ac1e4c4e12595a8947c51d088d52ba21700
Mohan Raj <mohan.raj@freshworks.com>
2021-05-21 01:10:23 +0530

    Cleanup

--- index.js
+++ index.js
@@ -5,29 +5,33 @@ const app = express();
 app.use(express.json());
 
 if (!(process.env.APP_ID && process.env.APP_CERTIFICATE)) {
-    throw new Error('You must define an APP_ID and APP_CERTIFICATE');
+  throw new Error("You must define an APP_ID and APP_CERTIFICATE");
 }
-var APP_ID = process.env.APP_ID;
-var APP_CERTIFICATE = process.env.APP_CERTIFICATE;
 
+const APP_ID = process.env.APP_ID;
+const APP_CERTIFICATE = process.env.APP_CERTIFICATE;
 
 app.post("/rtctoken", (req, res) => {
-  const appID = APP_ID;
-  const appCertificate = APP_CERTIFICATE;
-  const expirationTimeInSeconds = 3600;
   const uid = req.body.uid;
-  if (!uid) {
-        uid = 0;
-    }
-  const role = req.body.isPublisher ? Agora.RtcRole.PUBLISHER : Agora.RtcRole.SUBSCRIBER;
+  const role = req.body.isPublisher
+    ? Agora.RtcRole.PUBLISHER
+    : Agora.RtcRole.SUBSCRIBER;
   const channel = req.body.channel;
   const currentTimestamp = Math.floor(Date.now() / 1000);
-  const expirationTimestamp = currentTimestamp + expirationTimeInSeconds;
+  const expirationTimestamp = currentTimestamp + 3600;
 
-  const token = Agora.RtcTokenBuilder.buildTokenWithUid(appID, appCertificate, channel, uid, role, expirationTimestamp);
-  res.send({ uid, token });
+  const token = Agora.RtcTokenBuilder.buildTokenWithUid(
+    APP_ID,
+    APP_CERTIFICATE,
+    channel,
+    uid,
+    role,
+    expirationTimestamp
+  );
+  res.send(token);
 });
 
-
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Agora Auth Token Server listening at Port ${port}`));
+app.listen(port, () =>
+  console.log(`Agora Auth Token Server listening at Port ${port}`)
+);
